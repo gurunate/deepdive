727200f53ff58d9b3a4f8494919a5b6d9f939a1b
Ross Gerbasi <rgerbasi@gmail.com>
2013-11-09 16:27:22 -0600

    CleanUp and Added general topics

--- path.js
+++
@@ -1,6 +0,0 @@
-var path = require("path"),
-    replace = require("./replacer");
-    
-console.log(replace("this is one"));
-// console.log(__filename);
-// console.log(path.basename(__filename));
