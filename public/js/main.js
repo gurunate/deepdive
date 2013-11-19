ce9fad2eb9dea621e89630ec51b38b913e70ab63
Nate  Johnson <nathanalanjohnson@gmail.com>
2013-11-19 11:28:16 -0600

    Express setup

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/**
+ * @author Nate Johnson
+ */
+require.config({
+	baseUrl : 'js',
+
+	paths : {
+		jquery : ['//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min', 'libs/jquery-1.10.2.min'],
+		domReady : 'libs/require/domReady',
+		text : 'libs/require/text',
+		css : 'libs/require/css'
+	},
+	shim : {
+		// 'can' : ['jquery'],
+	}
+});
+
+require(['modules/chat', 'domReady!'], function(Chat) {
+	// TODO implement
+});
---
+++ public/js/modules/chat.js
@@ -0,0 +1,7 @@
+/**
+ * @author Nate Johnson
+ */
+define(['jquery', 'domReady!', 'css!/styles/chat.css'], function($) {
+	// TODO implement
+	console.log('AMD chat loaded');
+});
