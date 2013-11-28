a997105015795fa6f65de5f5ec1093e545e4c832
Nate  Johnson <nathanalanjohnson@gmail.com>
2013-11-28 16:40:04 -0600

    Add response button

--- public/js/modules/chat.js
+++ public/js/modules/chat.js
@@ -1,37 +1,48 @@
 /**
  * @author Cat in the Hat Johnson
  */
-define(['angular', 'domReady!', 'css!/css/chat.css'], function() {
-	var chatApp = angular.module('chatApp', []);
+ define(['angular', 'domReady!', 'css!/css/chat.css'], function() {
+ 	var chatApp = angular.module('chatApp', []).directive('initFocus', function() {
+ 		return function(scope, el, attr) {
+ 			el[0].focus();
+ 		};
+ 	});
 
-	chatApp.controller('chatCtrl', function ($scope) {
+ 	chatApp.controller('chatCtrl', function ($scope) {
 
-		$scope.messages = [
-			{
-				sender : 'Sam I am',
-				msg : 'Hello'
-			},
-			{
-				sender : 'Cat in the Hat',
-				msg : 'Hi'
-			},
-			{
-				sender : 'Cat in the Hat',
-				msg : 'Happy Thanksgiving!'
-			},
-			{
-				sender : 'Sam I am',
-				msg : 'Same to you.'
-			}
-		];
+ 		$scope.messages = [
+ 		{
+ 			sender : 'Sam I am',
+ 			msg : 'Hello'
+ 		},
+ 		{
+ 			sender : 'Cat in the Hat',
+ 			msg : 'Hi'
+ 		},
+ 		{
+ 			sender : 'Cat in the Hat',
+ 			msg : 'Happy Thanksgiving!'
+ 		},
+ 		{
+ 			sender : 'Sam I am',
+ 			msg : 'Same to you.'
+ 		}
+ 		];
 
-		$scope.chat = function(msg) {
-			$scope.messages.push({
-				sender : 'me',
-				msg : msg
-			});
+ 		$scope.chat = function(msg) {
+ 			$scope.messages.push({
+ 				sender : 'me',
+ 				msg : msg
+ 			});
 
-			$scope.msg = '';
-		}
-	});
-});
+ 			$scope.msg = '';
+ 		}
+
+ 		$scope.respond = function() {
+ 			$scope.messages.push({
+ 				sender : 'That guy',
+ 				msg : "Yep."
+ 			});
+ 		}
+ 	});
+ });
